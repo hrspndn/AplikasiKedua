45240c64b95527622b175a49c6a1ea0cc063b299
hrspndn <harispendinan05@gmail.com>
2020-05-03 17:00:15 +0700

    belajar react native

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View,Text}from 'react-native';
+const App = () => {
+  return(
+    <View style={{flex:1, flexDirection: 'row', backgroundColor: '#084596'}}>
+    <Text style= {{flex:1, textAlign: 'center', fontSize:35, fontWeight: 'normal', 
+    fontFamily: '', color:'white', lineHeight: 700 }}> 
+    This My First Apps :)  </Text>
+    </View>
+    
+  );
+}
+export default App;
