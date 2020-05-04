d0bee82d33c368b4b602e859d3dd75ac6bbca7bf
hrspndn <harispendinan05@gmail.com>
2020-05-04 22:13:03 +0700

    scrolview-image

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import {View,Text}from 'react-native';
-const App = () => {
+import {View,Text,Image,ScrollView} from 'react-native';
+// import scrollview
+
+ const App = () =>{
   return(
-    <View style={{flex:1, flexDirection: 'row', backgroundColor: '#084596'}}>
-    <Text style= {{flex:1, textAlign: 'center', fontSize:35, fontWeight: 'normal', 
-    fontFamily: '', color:'white', lineHeight: 700 }}> 
-    This My First Apps :)  </Text>
+    // menggunakan scroll
+    <ScrollView>
+      <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'red' }}>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'red'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'white'}}> HAI ?</Text>
+        {/* ini memanggil image secara local yang berada di folder assets */}
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require('./assets/1.jpg')}/>
+      </View>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'yellow'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'red'}}>APA KABAR DISANA ?</Text>
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require('./assets/2.jpg')}/>
+      </View>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'green'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'white'}}>AKU RINDU</Text>
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require('./assets/3.jpg')}/>
+      </View>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'blue'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'white'}}>KAMU </Text>
+        {/* ini memanggil image secara global yang ada di internent */}
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require( './assets/6.jpg')}/>
+      </View>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'purple'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'white'}}>BAIK BAIK DISANA</Text>
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require('./assets/4.jpg')}/>
+      </View>
+      <View style={{alignItems: 'center', width: '100%', height: 200, backgroundColor: 'white'}}>
+        <Text style={{ marginVertical: 10, fontSize: 20, color: 'black'}}>AKU SAYANG KAMU </Text>
+        <Image style={{ width: 200, height: 100, backgroundColor: 'red'}} source={require('./assets/5.jpg')}/>
+      </View>
     </View>
-    
+    </ScrollView>
   );
 }
+
 export default App;
